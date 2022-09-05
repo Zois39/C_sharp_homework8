50060edb0fd63f3c9d69f394bd709290d90e32de
Konstantin Bondarenko <zois1990@gmail.com>
2022-09-05 23:24:37 +0200

    task 60 complete

--- Program.cs
+++ Program.cs
@@ -164,6 +164,38 @@ Show2DArray(MatrixProduct(matrixA, matrixB));
 // 34(1,0,0) 41(1,1,0)
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
+/*
+int[,,] CreateRandom3DArray()
+{
+    int[,,] result = new int [2, 2, 2];
+    int[] uniqValue = new int[90];
+    int value;
+    for (int i = 0; i < result.GetLength(0); i++)
+        for (int j = 0; j < result.GetLength(0); j++)
+            for (int k = 0; k < result.GetLength(0); k++)
+            {
+                do
+                    value = new Random().Next(10, 100);
+                while (value == uniqValue[value - 10]);
+                uniqValue[value - 10] = value;
+                result[i, j, k] = value;
+            }
+    return result;
+}
+
+void Show3DArray (int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+            Console.Write($"{array[i, j, k]} {(i, j, k)}    ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+Show3DArray(CreateRandom3DArray());
+*/
 
 // Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
 // Например, на выходе получается вот такой массив:
